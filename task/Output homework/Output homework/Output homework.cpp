9c8d5d93efd2eabd0a0731ea984979958f2ccc30
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-01-22 22:16:18 +0300

    Add files via upload

---
+++ task/Output homework/Output homework/Output homework.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "*****************************************"<<std::endl;
+    std::cout << "Name : Raneem Alhorany\n";
+    std::cout << "Age : 21 years \n";
+    std::cout << "City : Amman\n";
+    std::cout << "Country : jordan\n";
+    std::cout << "*****************************************\n" << std::endl;
+
+    std::cout << "********\n";
+    std::cout << "********"<<std::endl;
+    std::cout << "********\n";
+    std::cout << "********\n"<<std::endl;
+
+    std::cout << "I love programming!\n"<<std::endl;
+    std::cout << "I promise to be the best developer ever!\n"<<std::endl;
+    std::cout << "I know it will take some time to practice, but i will achieve my goal.\n"<<std::endl;
+    std::cout << "Best Regards\n";
+    std::cout << "Raneem Alhorany\n\n";
+
+    std::cout << "*   *"<<std::endl;
+    std::cout << "*   *\n";
+    std::cout << "*****" << std::endl;
+    std::cout << "*   *\n";
+    std::cout << "*   *\n";
+
+
+
+}
