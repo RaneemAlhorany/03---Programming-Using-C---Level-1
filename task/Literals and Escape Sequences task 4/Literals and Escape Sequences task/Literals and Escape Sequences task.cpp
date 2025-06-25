466b05b1ef606fc91ab89f36ed94b5fc85343a2f
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-06-25 07:48:41 +0300

    Add files via upload

---
+++ task/Literals and Escape Sequences task 4/Literals and Escape Sequences task/Literals and Escape Sequences task.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "literals ::" << endl;
+    cout << "    are data  used for representing fixed values" << endl
+        << "    they  can be used directly in the code " << endl
+        << "    example --> 5 , you , ...." << endl
+        << "    it is the data that is stored in the program"
+        << "    you can not assign different values to these terms \n"
+        << "    This means that the number 5 is assigned to the variable\n"
+        << "    and does not sign anything inside it" << endl
+        << "-----------------------------------------------------------------------\n";
+
+    cout << "titerals types :\n"
+        << "    1)Floating point literals" << endl
+        << "    2)characters literals" << endl
+        << "    3)Integer Literals : " << endl
+        << "                         a)decemal" << endl
+        << "                         b)Octal =>  0--- (0 is prefex)" << endl
+        << "                         c)hexadecimal =>  0z--- (0z is prefex)" << endl
+        << "    4)Escape Sequence";
+        // // Escape sequences are used to represent characters that are not easily typed
+       // // or that have special meaning in the code.
+
+}
+
+
