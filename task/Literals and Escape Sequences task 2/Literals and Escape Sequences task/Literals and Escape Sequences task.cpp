950df2111c346c3e4685dd181b03fcb5a4650211
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-06-25 07:43:54 +0300

    Add files via upload

---
+++ task/Literals and Escape Sequences task 2/Literals and Escape Sequences task/Literals and Escape Sequences task.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Dear Sir\\Madam, \n"<<endl;
+    cout << "How are you?\n\n";
+    cout << "My name is \"Raneem\" , nice to meet you. \n\n";
+    return 0;
+}
+
+
