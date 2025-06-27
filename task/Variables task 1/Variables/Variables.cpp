0d256d5a8810746a50dc658635d1fb964a0a519b
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-06-27 15:43:23 +0300

    Add files via upload

---
+++ task/Variables task 1/Variables/Variables.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+   // specify if the name variable valid or not
+   // 1) Age
+            // valid
+   // 2) My-Age
+             //not valid  because use special character 
+   // 3) My_Age
+            // valid
+   // 4) _Age
+            // valid
+    // 5) 2X
+            //  not valid  because start with number        
+   // 6) X2
+            // valid 
+   // 7) My Name
+            // not valid because use white space 
+   // 8)  MyName
+            // valid
+   // 9) #Name
+            //not valid  because use special character 
+   // 10) $Name
+            //not valid  because use special character 
+   // 11) N@me
+            //not valid  because use special character 
+   // 12) int 
+            // not valid because use keyword
+   // 13) float
+            // not valid because use keyword
+
+}
