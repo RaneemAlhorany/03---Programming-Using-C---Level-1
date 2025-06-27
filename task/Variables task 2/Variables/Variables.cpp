161e0a417706dafdff3aa08f1ef987073a089a68
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-06-27 15:53:35 +0300

    Add files via upload

---
+++ task/Variables task 2/Variables/Variables.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name = "Raneem Alhorany";
+	int age = 21;
+	string city = "Amman";
+	string country = "Jordan";
+	float MonthlySalary = 75;
+	char gender = 'F';
+	bool isMarried = false;
+
+	cout << "********************************************\n";
+	cout << "Name: " << name << endl;
+	cout << "Age: " << age<< " years" << endl;
+	cout << "City: " << city << endl;
+	cout << "Country: " << country << endl;
+	cout << "Monthly Salary : " << MonthlySalary << endl;
+	cout << "Yearly Salary : " <<  MonthlySalary * 12 << endl;
+	cout << "Gender : " << gender << endl;
+	cout << "Married : " << isMarried << endl;
+	cout << "********************************************\n";
+
+}
+
