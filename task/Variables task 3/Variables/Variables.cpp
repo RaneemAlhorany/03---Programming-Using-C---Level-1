a30c2efbc982516ade81e719bf0ec762ee42c18d
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-06-27 16:00:56 +0300

    Add files via upload

---
+++ task/Variables task 3/Variables/Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number1 = 20 , number2 = 30 ,number3 = 10;
+	int summation_3_number = number1 + number2 + number3;
+
+	cout << number1 << "+ "<< endl;
+	cout << number2 << "+ "<< endl;
+	cout << number3 << endl;
+	cout << "------------------------------------\n" ;
+	cout << summation_3_number;
+}
+
