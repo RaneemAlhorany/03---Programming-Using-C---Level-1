5509083b6cbf94232a41ef7155be20068ae5750f
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-07-10 05:06:41 +0300

    Add files via upload

---
+++ task/input_user task 3/input_user/input_user.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int age = 22;
+
+	cout << "enter your age: ";
+	cin >> age;
+
+	cout << "After 5 years you will be " << age + 5 << " years old.";
+	return 0;
+}
+
