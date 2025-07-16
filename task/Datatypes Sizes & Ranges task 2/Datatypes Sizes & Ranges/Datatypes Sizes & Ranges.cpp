9d30be60fe47d45db044d3e66dffbb0d3c8e436e
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-07-16 16:41:22 +0300

    Add files via upload

---
+++ task/Datatypes Sizes & Ranges task 2/Datatypes Sizes & Ranges/Datatypes Sizes & Ranges.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short  number1 = 20, number2 = 30, number3 = 10;
+	short summation_3_number = number1 + number2 + number3;
+
+	cout << number1 << "+ " << endl;
+	cout << number2 << "+ " << endl;
+	cout << number3 << endl;
+	cout << "------------------------------------\n";
+	cout << summation_3_number;
+}
