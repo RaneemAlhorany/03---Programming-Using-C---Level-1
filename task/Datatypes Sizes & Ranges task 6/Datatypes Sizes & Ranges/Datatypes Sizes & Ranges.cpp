8b1e4c7cbb06573a962a436e6e9b731af8436012
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-07-16 16:46:27 +0300

    Add files via upload

---
+++ task/Datatypes Sizes & Ranges task 6/Datatypes Sizes & Ranges/Datatypes Sizes & Ranges.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short  age = 22;
+
+	cout << "enter your age: ";
+	cin >> age;
+
+	cout << "After 5 years you will be " << age + 5 << " years old.";
+	return 0;
+}
