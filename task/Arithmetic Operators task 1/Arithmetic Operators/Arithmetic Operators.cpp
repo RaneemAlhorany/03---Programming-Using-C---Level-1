0f97f481dffb83d607b28c03c022e099c3d6175f
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:42:17 +0300

    Add files via upload

---
+++ task/Arithmetic Operators task 1/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short number1, number2;
+
+	cout << "Please  enter the first number A?" << endl;
+	cin >> number1;
+	cout << "Please  enter the second number B?" << endl;
+	cin >> number2;
+
+	cout << number1 << " + " << number2 << " = " << number1 + number2<<endl;
+	cout << number1 << " - " << number2 << " = " << number1 - number2 << endl;
+	cout << number1 << " * " << number2 << " = " << number1 * number2 << endl;
+	cout << number1 << " / " << number2 << " = " << number1 / number2 << endl;
+	cout << number1 << " % " << number2 << " = " << number1 % number2 << endl;
+
+
+
+}
+
+
