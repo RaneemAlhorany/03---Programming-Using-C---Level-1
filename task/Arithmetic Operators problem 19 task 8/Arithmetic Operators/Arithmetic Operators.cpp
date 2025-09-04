802847ab01b33ced4642c239150633a282066ef1
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:49:30 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 19 task 8/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const float PI = 3.14;
+	int		diameter;
+
+
+	cout << "Enter the diameter value:\n";
+	cin >> diameter;
+
+	cout << "the value of circle Area through diameter :"<< (PI * diameter * diameter / 4)<<endl;
+
+
+}
+
+
