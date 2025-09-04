95f04c551fc9af842cdec320b1897a154d082196
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:43:26 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 7 task 2/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short num;
+
+	cout << "plz enter the number \n";
+	cin >> num;
+	cout << "Half of " << num << " is " << num / 2;
+
+
+}
+
+
