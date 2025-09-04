1f6a98cab4baa5f6c6b1616812e91e7968b5275f
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:45:38 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 10 task 4/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short mark1 , mark2, mark3;
+
+	cout << "plz enter the first mark \n";
+	cin >> mark1;
+	cout << "plz enter the second mark \n";
+	cin >> mark2;
+	cout << "plz enter the third mark \n";
+	cin >> mark3;
+
+	cout << "The Average of marks  " << (mark1+mark2 +mark3)/3 <<endl;
+
+
+}
+
+
