fa9ec26c89821aa31c7ef448a2e723cce686d3ea
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:44:25 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 9 task 3/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	short number1 , number2 , number3;
+
+	cout << "plz enter the first number \n";
+	cin >> number1;
+	cout << "plz enter the second number \n";
+	cin >> number2;
+	cout << "plz enter the third number \n";
+	cin >> number3;
+
+	cout << "summation of numbers is " << number1+number2 +number3 <<endl;
+
+
+}
+
+
