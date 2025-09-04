221355b9266dd85804af803e2c2516cae102a05e
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:48:29 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 17 task 7/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int		length_of_the_base, height;
+
+	cout << "Enter the length of the base value:\n";
+	cin >> length_of_the_base;
+	cout << "Enter the height value:\n";
+	cin >> height;
+
+	cout << "the value of Triangle  Area :"<< (.5* length_of_the_base * height)<<endl;
+
+
+}
+
+
