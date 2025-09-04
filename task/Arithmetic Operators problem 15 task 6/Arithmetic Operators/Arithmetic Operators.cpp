adf41a8ca05bbf9d568cd45fed0d7c74d0193ec5
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-09-04 16:47:28 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 15 task 6/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int		width , height;
+
+	cout << "Enter the width value:\n";
+	cin >> width;
+	cout << "Enter the height value:\n";
+	cin >> height;
+
+	cout << "the value of Rectangle Area :"<< (width*height)<<endl;
+
+
+}
+
+
