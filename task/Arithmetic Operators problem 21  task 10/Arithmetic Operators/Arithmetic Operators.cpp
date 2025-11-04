40a7b00dd746fdb5ff073d0c1bd9581a9c57cb0d
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-11-04 08:17:26 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 21  task 10/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int  circumference;
+	float Area;
+	const float PI = 3.14;
+	cout << "claculate circle Area along the circumference:" << "\n";
+	cout << "Enter Square the circumference" << "\n";
+	cin >> circumference;
+	Area = (circumference * circumference) / (4 * PI);
+	cout << "the circle Area along the circumference: " << Area << endl;
+
+
+
+
+
+
+
+
+
+
+}
+
+
