de894022c8e88d95ffc499b691002e96b22976e8
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-01-10 15:27:18 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 31  task 12/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int  number , numOf2 , numOf3 , numOf4;
+	cout << "calculate the power of 2 ,3 , 4" << "\n";
+	cout << "Enter  the number" << "\n";
+	cin >> number;
+	numOf2 = number * number;
+	numOf3 = numOf2 * number;
+	numOf4 = numOf3 * number;
+	cout << "the power of 2 :" << numOf2 << "\nthe power of 3: " << numOf3 << "\nthe power of 4: " <<numOf4 <<endl;
+
+
+
+
+
+
+
+
+
+}
+
+
