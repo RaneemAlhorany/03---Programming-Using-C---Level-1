8a52e5736b2f64929debf12f40a30450381b087c
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-01-14 22:57:08 +0300

    Add files via upload

---
+++ task/Arithmetic Operators problem 47  task 18/Arithmetic Operators/Arithmetic Operators.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int Loan_Amount , Monthly_Payment ;
+
+	cout << "enter the Loan Amount: \n";
+	cin >> Loan_Amount;
+	cout << "enter the Monthly Payment: \n";
+	cin >> Monthly_Payment;
+
+	
+	cout << Loan_Amount / Monthly_Payment << " Months" << endl;
+
+	
+
+
+
+
+
+
+
+}
+
+
