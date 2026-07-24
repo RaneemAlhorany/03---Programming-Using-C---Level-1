7f83b1c203c021620622c08ab32a855c49e057ab
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2026-07-24 20:37:42 +0300

    Add files via upload

---
+++ task/Nested For Loops task 3/Nested For Loops/Nested For Loops.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
+void printReverseNumbers()
+{
+    for (int i = 10 ; i > 0 ; i--)
+    {
+        for (int j = 1 ; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+}
+
+
+int main()
+{
+    printReverseNumbers();
+
+	return 0;
+}
